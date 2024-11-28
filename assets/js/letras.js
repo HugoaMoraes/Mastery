6bd95f255c788ef0b025d233c402ab885c2472b9
Hugo Moraes <hugomoraes2007@gmail.com>
2024-11-28 17:45:13 -0300

    primeiro commit

---
+++ assets/js/letras.js
@@ -0,0 +1,44 @@
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const phrases = [
+  "O futuro do front-end começa aqui!",
+  "De desenvolvedor a inovador em 2025!",
+  "Conhecimento prático para criar interfaces incríveis!",
+  "Tendências e técnicas para dominar o front-end!",
+  "Prepare-se para o que está por vir no mundo web!",
+];
+const el = document.getElementById("typewriter");
+
+let sleepTime = 100;
+
+let curPhraseIndex = 0;
+
+const writeLoop = async () => {
+  while (true) {
+    let curWord = phrases[curPhraseIndex];
+
+    for (let i = 0; i < curWord.length; i++) {
+      el.innerText = curWord.substring(0, i + 1);
+      await sleep(sleepTime);
+    }
+
+    await sleep(sleepTime * 10);
+
+    for (let i = curWord.length; i > 0; i--) {
+      el.innerText = curWord.substring(0, i - 1);
+      await sleep(sleepTime);
+    }
+
+    await sleep(sleepTime * 5);
+
+    if (curPhraseIndex === phrases.length - 1) {
+      curPhraseIndex = 0;
+    } else {
+      curPhraseIndex++;
+    }
+  }
+};
+
+writeLoop();
