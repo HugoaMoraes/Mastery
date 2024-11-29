620742f512f97ac5fe5ea0f11ecbfc3d2308607f
Hugo Moraes <hugomoraes2007@gmail.com>
2024-11-28 21:42:02 -0300

    atualizações css e button color

---
+++ assets/js/temas.js
@@ -0,0 +1,19 @@
+const temas = ['assets/css/theme-color/bridge-theme.css',
+                'assets/css/theme-color/default-theme.css',
+                'assets/css/theme-color/green-theme.css',
+                'assets/css/theme-color/lite-blue-theme.css',
+                'assets/css/theme-color/purple-theme.css',
+                'assets/css/theme-color/red-theme.css'            
+            ];
+
+        function trocarTemaAleatorio() {
+            // Seleciona um tema aleatório da lista
+            const temaAleatorio = temas[Math.floor(Math.random() * temas.length)];
+            
+            // Atualiza o link do tema
+            const link = document.getElementById('theme-style');
+            link.setAttribute('href', temaAleatorio);
+
+            // Exibe o tema no console (opcional)
+            console.log(`Tema trocado para: ${temaAleatorio}`);
+}
